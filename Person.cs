f61d873199c977a5a3d9c26726ca25dc2ba63246
Nicolás Alliaume <nicolasalliaume@gmail.com>
2020-03-30 19:07:34 -0300

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,34 @@
+using System; 
+ 
+namespace PosterObjectsClassesMessages 
+{ 
+    public class Person 
+    { 
+        public string name; 
+
+        public string id; 
+
+        public Person(string name, string id) 
+        { 
+            This.name = name; 
+            This.id = id; 
+        } 
+
+        public void IntroduceYourself() 
+        { 
+            Console.WriteLine( 
+                $"Soy {this.name} y mi cédula es {this.id}"); 
+        } 
+    } 
+
+    class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            Person john = new Person("John Doe", "1.234.567-8"); 
+            Person jane = new Person("Jane Doe", "8.765.432-1"); 
+            john.IntroduceYourself(); 
+            jane.IntroduceYourself(); 
+        } 
+    } 
+}
